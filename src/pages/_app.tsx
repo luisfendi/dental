2c053714c68e6c6ae98b4cf2c52833dab9f5e313
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-31 17:47:40 +0300

    style: created varsiabless and main styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import '../styles/main.scss';
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
+  return <Component {...pageProps} />;
 }
---
+++ src/pages/title.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React, { useEffect, useState } from 'react';
+import s from './title.module.scss';
+
+export const Title = () => (
+  <div className='bg'>
+    <h1 className={s.title}>TITLE</h1>
+  </div>
+);
